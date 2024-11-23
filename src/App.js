2920917ce7bd0eb61736a8f17058c9e8a0bab3aa
Hello-Hos <ritikritik0@gmail.com>
2024-11-23 22:08:06 +0530

    updateee

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Header from "./components/Header";
@@ -8,8 +8,19 @@ import Contact from './components/Contact';
 import Error from "./components/Error";
 import RestaurantMenu from './components/RestaurantMenu';
 import Footer from './components/Footer';
+import logo from "./utils/logo.png"; // Import the favicon
 
+// AppLayout component
 const AppLayout = () => {
+    // UseEffect to dynamically set the favicon
+    useEffect(() => {
+        const link = document.createElement('link');
+        link.rel = 'icon';
+        link.type = 'image/png';
+        link.href = logo; // Set the imported favicon
+        document.head.appendChild(link);
+    }, []);
+
     return (
         <div className="app min-h-screen flex flex-col">
             <Header />
@@ -48,5 +59,6 @@ const appRouter = createBrowserRouter([
     },
 ]);
 
+// Render the app
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter} />);
